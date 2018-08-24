38fd4ea4c1a410ed320b0e1e4557898dfc2cc80d
Colin Qiao <colinq@gmail.com>
2018-08-23 17:09:05 -0700

    fix

--- queryplan.sh
+++ queryplan.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # Use this script to load the iReceptor query plans into the mongodb
-
-./export.sh
 # the irdn-mongo container should be running before the following command
 sudo docker exec -it irdn-mongo mongo --authenticationDatabase admin $MONGODB_DB -u $MONGODB_SERVICE_USER -p $MONGODB_SERVICE_SECRET /dbsetup/queryplan.js
